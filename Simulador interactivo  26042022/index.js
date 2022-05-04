d8db7990bed7fa200d19986b4dd2d392b5bf6b6d
JulioHudson <100172036+JulioHudson@users.noreply.github.com>
2022-05-03 23:58:47 -0300

    entrega 04/05

--- Simulador interactivo  26042022/index.js
+++ Simulador interactivo  26042022/index.js
@@ -1,13 +1,27 @@
-const mug = 300;
-const book = 250;
-const draw = 500;
-const pic = 400;
+const products = []
 
-let msg1 = "Que producto desea adquirir?" + "\n" +
-           "1 - Tasa con dibujo y leyenda personalizada" + "\n" +
-           "2 - Libro de ilustraciones" + "\n" +
-           "3 - Dibujo personalizado" + "\n" +
-           "4 - Imagen a eleccion con texto personalizado" + "\n";
+class Product {
+    constructor (name,type,price){
+        this.name = name;
+        this.type = type;
+        this.price = price;
+    }   
+}
+
+const mug = new Product("Mug","Marchandice",300);
+const book = new Product("Book","Marchandice",250);
+const draw = new Product("Draw","Marchandice",500);
+const pic = new Product("Picture","Marchandice",400);
+
+products.push(mug,book,draw,pic);
+
+const payments = [30,20,0]
+
+let msg1 = "Wich product would you like tu buy?" + "\n" +
+           "1 - Mug with custom image and text" + "\n" +
+           "2 - Ilustration book" + "\n" +
+           "3 - Personalized draw" + "\n" +
+           "4 - Select image with custom text" + "\n";
 
 let menu1 = () => parseInt(prompt(msg1));
 let product = menu1(); 
@@ -17,9 +31,9 @@ if (product < 1 || product > 4 || isNaN(product)) {
     product = menu1(); 
 }
 
-let msg = "Ingrese el metodo de pago" + "\n" +
-           "1 - Transferencia bancaria" + "\n" +
-           "2 - Tarjeta de credito" + "\n" +
+let msg = "wich payment method do you want to use" + "\n" +
+           "1 - Wired transfer" + "\n" +
+           "2 - Credit card" + "\n" +
            "3 - Mercadopago" + "\n";
 
 let menu = () => parseInt(prompt(msg));
@@ -30,85 +44,16 @@ if (result < 1 || result > 3 || isNaN(result)) {
     result = menu(); 
 }
 
+
 function discount(price,percent) {
     return price - ((price * percent) / 100);
 }
-switch (product) {
-    case 1:
-        product = mug;
-        document.write("Usted adquirio una Tasa con dibujo y leyenda personalizada");
-        switch (result) {
-            case 1:
-                alert("El pago por transferencia bancaria tiene un descuento del 30%, el precio final del producto es de " + discount(product,30));
-                break;
-            case 2: 
-                alert("El pago por tarjeta de credito no posee descuento, el precio final del producto es de " + product);
-                break;
-            case 3:
-                alert("El pago por MercadoPago tiene un descuento del 20%, el precio final del producto es de " + discount(product,20));
-                break;
-            default:
-                break;
-            }
-        break;
-    
-    case 2:
-        product = book;
-        document.write("Usted adquirio un libro de ilustraciones");
-        switch (result) {
-            case 1:
-                alert("El pago por transferencia bancaria tiene un descuento del 30%, el precio final del producto es de " + discount(product,30));
-                break;
-        
-            case 2:
-                alert("El pago por tarjeta de credito no posee descuento, el precio final del producto es de " + product);
-                break;
-            
-            case 3:
-                    alert("El pago por MercadoPago tiene un descuento del 20%, el precio final del producto es de " + discount(product,20));
-                    break;
-        
-            default:
-                break;
-            }
-        break; 
+console.log(products[product-1])
+let productName = products[product-1].name;
+let paymentDisc = payments[result-1];
+let productPrice = products[product-1].price;
 
-    case 3:
-        product = draw;
-        document.write("Usted adquirio un dibujo personalizado");
-        switch (result) {
-            case 1:
-                alert("El pago por transferencia bancaria tiene un descuento del 30%, el precio final del producto es de " + discount(product,30));
-                break;
-        
-            case 2:
-                alert("El pago por tarjeta de credito no posee descuento, el precio final del producto es de " + product);
-                break;
-            
-            case 3:
-                    alert("El pago por MercadoPago tiene un descuento del 20%, el precio final del producto es de " + discount(product,20));
-                    break;
-        
-            default:
-                break;
-            }
-    break;
-    
-    case 4:
-        product = pic;
-        document.write("Usted adquirio una imagen a eleccion con texto personalizado");
-        switch (result) {
-            case 1:
-                alert("El pago por transferencia bancaria tiene un descuento del 30%, el precio final del producto es de " + discount(product,30));
-                break;
-            case 2:
-                alert("El pago por tarjeta de credito no posee descuento, el precio final del producto es de " + product);
-                break;
-            case 3:
-                    alert("El pago por MercadoPago tiene un descuento del 20%, el precio final del producto es de " + discount(product,20));
-                    break;
-            default:
-                break;
-            }
-    break;    
-}
+
+
+alert("El producto " + productName + " tiene un descuento de " + paymentDisc + " siendo el precio final de " + discount(productPrice,paymentDisc));
+
